d43ce45934f59456cfdb01843c136a016f0d7812
석현07 <92662236+sunpark123@users.noreply.github.com>
2025-10-29 10:58:11 +0900

    server

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Dockerfile
-FROM python:3.11-slim
-
-WORKDIR /app
-COPY . .
-
-# RUN pip install --no-cache-dir -r requirements.txt
-
-CMD ["python", "app.py"]
---
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        System.out.println("ok");
+        return "test.html";
+    }
+}
